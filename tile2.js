facea7aabfe8fdac50802525ff6de8628277d4e2
Max Glotov <max@maxispecs.com>
2024-07-30 17:51:35 -0700

    allineone

--- tile2.js
+++ tile2.js
@@ -5,10 +5,55 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 // import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-import { setupRenderer, setupControls, setupLights } from './js/scene.js';
-import { windowResize } from "./js/events.js";
+// import { setupRenderer, setupControls, setupLights } from './js/scene.js';
+// import { windowResize } from "./js/events.js";
+
+export const windowResize = (showcase) => () => {
+    // showcase.camera.aspect = window.innerWidth / window.innerHeight;
+    showcase.camera.aspect = showcase.container.offsetWidth / showcase.container.offsetHeight;
+    showcase.camera.updateProjectionMatrix();
+    // showcase.renderer.setSize(window.innerWidth, window.innerHeight);
+    showcase.renderer.setSize(showcase.container.offsetWidth, showcase.container.offsetHeight);
+}
+
+
+
+export const setupControls = (showcase) => {
+    let controls = new OrbitControls(showcase.camera, showcase.orbitControlsContainer);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
+    return controls;
+}
+
+
+export const setupRenderer = () => {
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true }); // Enable alpha channel
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 0); // Set background color to transparent
+    renderer.domElement.style.position = 'absolute';
+    renderer.domElement.style.zIndex = 3; // Ensure the canvas is on top
+    return renderer;
+
+}
+
+
+
+export const setupLights = (showcase) => {
+
+    // Lighting
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
+    showcase.scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.set(5, 5, 5);
+    showcase.scene.add(pointLight);
+
+}
+
 
 
 const showcase = {
